8eccf8e586cda2c1442932d821456ae8e5e86454
yuan liu <1623551734@qq.com>
2022-03-08 00:15:06 +0800

    nothing

--- martianmips.v
+++ martianmips.v
@@ -3,7 +3,26 @@
 
 module martianmips (
     input   wire    clk,
-    input   wire    rst
+    input   wire    rst,
+
+    input   wire[`RegDataBus]   rom_data_i,
+    output  reg[`RegDataBus]    rom_addr_o,
+    output  wire                rom_ce_o
 );
+
+//连接IF/ID模块与ID模块的变量
+wire[`InstAddrBus]  pc;
+
+
+
+//pc_reg模块例化
+pc_reg  pc_reg0(
+    .clk(clk),
+    .rst(rst),
+    .pc(pc),
+    .ce(rom_ce_o)
+);
+
+
 
 endmodule //martianmips
